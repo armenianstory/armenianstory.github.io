d4a482cf714d267797917c6a4f239b4d9f7ab77f
armenianstory <111173685+armenianstory@users.noreply.github.com>
2022-08-13 12:08:34 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+   function openit(){
+                document.getElementById("overlay").style.width = "100%";                document.getElementById("buti").style.display = "block";
+                document.getElementById("mahver").style.display = "block";
+           
+                
+            }
+            function closeit(){
+                document.getElementById("overlay").style.width = "0";   
+                document.getElementById("buti").style.display = "none";
+                document.getElementById("mahver").style.display = "none";
+            }
+            setTimeout(
+            function showw(){
+                document.getElementById("h112").style.opacity = "1";
+            },1000)
+            
